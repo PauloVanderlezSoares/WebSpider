020f5721229580880fe0ecc3a3977b895a32878e
Paulo Soares <pauloavsoares17@gmail.com>
2021-11-29 19:22:06 -0300

    segundo commit

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # WebSpider
 
- # main_webcrawler.py
+ ### main_webcrawler.py
  é o arquivo principal que aciona o WebSpider
 
- # web_crawler.py
- é o arquivo com o código do objeto WebSpider
+ ### web_crawler.py
+ - é o arquivo com o código do objeto WebSpider
 
- # teste_web_crawler.py
- é o arquivo com o teste de requisição do site
+ ### teste_web_crawler.py
+ - é o arquivo com o teste de requisição do site
 
- python3 -m pytest teste_web_crawler.py  -->> para rodar o teste
+ - python3 -m pytest teste_web_crawler.py  -->> para rodar o teste
+
+ ### WebSpider.webcrawler_output()
+ - exporta lista de sites secundários em um arquivo .txt
--- web_crawler.py
+++ web_crawler.py
@@ -16,9 +16,40 @@ class WebSpider:
 
     def title_page(self):
         title = self.tags.find("title")
-        print(title.text)
+        print(f"Página principal: {title.text}")
 
     def webcrawler(self):
+
+        allLinks = self.tags.find("html").find_all("a")
+
+        lista_links = []
+        for link in allLinks:
+            x = str(link).split(" ")
+            for y in x:
+                if 'href' and '/wiki/' in y:
+                    lista_links.append(y)
+
+        lista_wiki = []
+        for link in lista_links:
+            link = link.split('"')
+            for link_valido in link:
+                if "/wiki/" in link_valido:
+                    lista_wiki.append(link_valido)
+
+        for link in lista_wiki:
+            if 'https' in link:
+                resposta = get(link)
+                tags = BeautifulSoup(resposta.text, "html5lib")
+                title = tags.find("title")
+                print(f"Página secundária: {title.text}")
+
+            else:
+                resposta = get('https://pt.wikipedia.org' + link)
+                tags = BeautifulSoup(resposta.text, "html5lib")
+                title = tags.find("title")
+                print(f"Página secundária: {title.text}")
+
+    def webcrawler_output(self):
         allLinks = self.tags.find("html").find_all("a")
 
         lista_links = []
